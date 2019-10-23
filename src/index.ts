efd820a350821aeb96ce43988356ffadbaf5a64b
Brian Lauber <brian.lauber@33across.com>
2019-10-23 13:07:00 -0400

    Typescript declarations are auto-generated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 
 import ES6Error from 'es6-error';
 
-import {
-  Conditional,
-  DispatcherInstance,
-  Rule,
-  Handler,
-} from '../typings/index';
+
+export interface Conditional<INPUT extends any[]> {
+  (...args: INPUT): boolean,
+}
+
+export interface Handler<INPUT extends any[], OUTPUT> {
+  (...args: INPUT): OUTPUT,
+}
+
+export interface Rule<INPUT extends any[], OUTPUT> {
+  (...args: INPUT): Handler<INPUT, OUTPUT>,
+}
+
+export interface DispatcherInstance<INPUT extends any[], OUTPUT> {
+  (...args: INPUT): OUTPUT,
+
+  rules: Rule<INPUT, OUTPUT>[],
+  onMatchFailure: Handler<INPUT, OUTPUT>,
+  use(rule: Rule<INPUT, OUTPUT>): DispatcherInstance<INPUT, OUTPUT>,
+  otherwise(handler: Handler<INPUT, OUTPUT>): DispatcherInstance<INPUT, OUTPUT>,
+}
+
 
 
 
