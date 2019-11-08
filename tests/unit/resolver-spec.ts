276bddab2dfe732a4f511116c252ffc96f2b6457
Brian Lauber <constructible.truth@gmail.com>
2019-11-08 14:36:08 -0500

    Spec to verify DepthFirstResolver participants

--- tests/unit/resolver-spec.ts
+++ tests/unit/resolver-spec.ts
@@ -4,9 +4,10 @@ import {expect} from 'chai';
 import {
   DepthFirstResolver,
 } from '../../src/resolver';
+import { Resolver } from 'dns';
 
 describe('DepthFirstResolver', function() {
-  it('works', function() {
+  it('yields values in a depth-first order', function() {
     function *itemsResolver() {
       yield "get";
       yield "drop";
@@ -28,5 +29,49 @@ describe('DepthFirstResolver', function() {
       "get", "drop", "items", "go", "teleport",
     ]);
 
+  });
+
+  it('allows Resolver participants to be chosen', function() {
+    function* foo(x: number) {
+      for(let i = 0; i < x; ++i) {
+        yield i;
+      }
+    }
+
+    function *bar(x: number) {
+      for(let i = x; i < 10; ++i) {
+        yield i;
+      }
+    }
+
+    const resolve = DepthFirstResolver<[number], number>(
+      function* getResolvers(x: number) {
+        if(x < 4) {
+          yield foo;
+        }
+
+        if(x > 2) {
+          yield bar;
+        }
+      }
+    );
+
+    // Only `foo` is participating
+    expect(Array.from(resolve(2))).to.deep.equal([
+      0, 1,
+    ]);
+
+    // Both `foo` and `bar` are participating
+    expect(Array.from(resolve(3))).to.deep.equal([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    ]);
+
+    // Only `bar` is participating
+    expect(Array.from(resolve(5))).to.deep.equal([
+      5, 6, 7, 8, 9,
+    ]);
+
+    // No resolvers are participating
+    expect(Array.from(resolve(NaN))).to.deep.equal([]);
   });
 });
